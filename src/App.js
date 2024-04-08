0f39f86776d8efdbb958f1b2889c0d31d068496b
sahtushar <sahtushar@gmail.com>
2024-04-08 21:37:19 +0530

    changes made

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 
 import io from "socket.io-client";
 
-const socket = io("http://localhost:4000", {
+const socket = io(process.env.SERVER_URL || "http://localhost:4000", {
   withCredentials: true,
 });
 
